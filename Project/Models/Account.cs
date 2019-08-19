07a4ad327df1ba0d0dd62264591ccb4b77eddbe3
daniel <arutyunyan.daniel23@mail.ru>
2019-08-19 23:00:36 +0300

    Change ms sql db on local db.

--- Project/Models/Account.cs
+++ Project/Models/Account.cs
@@ -1,23 +1,22 @@
 namespace Project.Models
 {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
     [Table("Account")]
     public partial class Account
     {
         public Guid Id { get; set; }
 
+        [Required]
         [StringLength(120)]
-        [Display(Name = "Email")]
-        [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
+        [Required]
         [StringLength(120)]
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
         [Required]
@@ -28,7 +27,6 @@ namespace Project.Models
         [StringLength(120)]
         public string LastName { get; set; }
 
-        [Required]
         [StringLength(120)]
         public string Country { get; set; }
     }
